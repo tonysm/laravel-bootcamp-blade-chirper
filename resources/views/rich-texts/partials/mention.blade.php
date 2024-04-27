d66eecf7cb0c4e350048ecfd347b6000f96e0ee3
Tony Messias <tonysm@hey.com>
2024-04-26 21:07:28 -0300

    pretend there are images

--- resources/views/rich-texts/partials/mention.blade.php
+++ resources/views/rich-texts/partials/mention.blade.php
@@ -1,1 +1,4 @@
-<span class="underline underline-offset-2 text-indigo-600">{{ $user->name }}</span>
+<span class="relative whitespace-nowrap !pl-5">
+    <span class="absolute left-0 inset-y-0 h-5 w-5 rounded-full bg-indigo-500 shadow-sm"></span>
+    <span class="whitespace-nowrap">{{ $user->name }}</span>
+</span>
