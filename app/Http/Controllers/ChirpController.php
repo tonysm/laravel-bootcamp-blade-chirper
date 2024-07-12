ae65e432b064912fbada431c6fbd9d0f390501eb
Tony Messias <tonysm@hey.com>
2024-07-12 17:25:39 -0300

    Deleting chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -81,6 +81,10 @@ class ChirpController extends Controller
      */
     public function destroy(Chirp $chirp)
     {
-        //
+        Gate::authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
     }
 }
