12d9a6064a1509e6b134fb90737e352b00d4c70f
Tony Messias <tonysm@hey.com>
2024-07-12 17:14:08 -0300

    Creating chirps

---
+++ resources/views/chirps/index.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <form method="POST" action="{{ route('chirps.store') }}">
+            @csrf
+            <textarea
+                name="message"
+                placeholder="{{ __('What\'s on your mind?') }}"
+                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full"
+            >{{ old('message') }}</textarea>
+            <x-input-error :messages="$errors->get('message')" class="mt-2" />
+            <x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
+        </form>
+    </div>
+</x-app-layout>
