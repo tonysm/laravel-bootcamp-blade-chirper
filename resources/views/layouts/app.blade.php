ff1bb3cd5b422cde5ad801135892fad3f5ec31c6
Tony Messias <tonysm@hey.com>
2025-01-24 13:17:32 -0300

    Install Rich Text Laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <x-rich-text::styles theme="richtextlaravel" data-turbo-track="false" />
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
